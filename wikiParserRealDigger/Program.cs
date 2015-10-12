0cffd48865cf67e278f459078d73a530a456daf0
Lenchickk <elena.labzina@lenchick.net>
2015-10-12 14:51:44 -0500

    main_version

--- wikiParserRealDigger/Program.cs
+++ wikiParserRealDigger/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 
-namespace wikiParserRealDigger
+namespace vpUltimate
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            String viewDirectory = @"C:\Users\Lenchick\Google Drive\WASHU FALL2015\play ground\pipe\test\";
+            String[] viewsFiles = System.IO.Directory.GetFiles(viewDirectory, "*_in.csv");
+            BunchHelper.Start = @"01/2011";
+            BunchHelper.End = @"09/2015";
+
+            foreach (String file in viewsFiles)
+            {
+                BunchHelper helper = new BunchHelper();
+                helper.processPipeFile(file);
+            }
+
+
         }
     }
 }
